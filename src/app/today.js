5dd23c39ac67b2b43c63e55db2b28732de7cd514
iwhozaifa <alihuzaifa7204@gmail.com>
2026-08-09 01:29:02 +0500

    Added today only tasks logic

---
+++ src/app/today.js
@@ -0,0 +1,43 @@
+import { loadFromLocalStorage ,getActiveTodos } from "./completed.js"; 
+import { createCard } from "./all-todos.js"
+const contentArea = document.getElementById("content")
+
+export function showToday(){
+  loadFromLocalStorage();
+
+  contentArea.innerHTML =  `
+  <div class="header">
+    <h3>Today's Todos</h3>
+  </div>
+  `
+  let cardContainer = document.querySelector(".cardContainer")
+  if (!cardContainer) {
+    cardContainer = document.createElement("div");
+    cardContainer.classList.add("cardContainer");
+    contentArea.appendChild(cardContainer);
+  }
+  
+  const todayTasks = returnFilteredTodos();
+
+  todayTasks.forEach(element => {
+    createCard(element,cardContainer)
+  });
+}
+
+function returnFilteredTodos (){
+
+    let activeTodos = getActiveTodos();
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2,"0");
+    const day = String(today.getDate()).padStart(2,"0");
+    
+    const formattedDate = `${year}-${month}-${day}`
+    activeTodos = activeTodos.filter((item) => item.date === formattedDate);
+    
+    console.log("active todays tasks:", activeTodos)
+    
+    return activeTodos;
+}
+
+
--- src/main/index.js
+++ src/main/index.js
@@ -3,11 +3,14 @@ import "../styles/allPage.css"
 import "../styles/card.css"
 import {renderCompleted} from "../app/completedTodos.js"
 import {addTodo, render, } from "../app/all-todos.js"
+import { showToday } from "../app/today.js"
 
 const allBtn = document.getElementById("all")
 const completedBtn = document.getElementById("completed")
+const todayBtn = document.getElementById("today")
 
 render();
 
 allBtn.addEventListener("click", render);
 completedBtn.addEventListener("click",renderCompleted);
+todayBtn.addEventListener("click", showToday)
